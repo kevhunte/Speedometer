f5ffda81507d91c48d6db6d915811bbfd2a9143e
kevhunte <kevinmhunte@gmail.com>
2018-05-11 15:19:37 -0400

    First commit for speedometer project

---
+++ app/src/main/java/kevinhunte/speedometer/DisplayMessageActivity.java
@@ -0,0 +1,82 @@
+package kevinhunte.speedometer;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    //found from a YouTube video
+    private LocationManager locationManager;//used to access gps
+    private LocationListener locationListener;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {//import needed functions for gps, use to make function and return speed
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {//if the gps location changes
+                textView.append("\n"+location.getLatitude()+ " "+location.getLongitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {//if gps is turned off, offer option to turn on
+                Intent _intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);//goes to settings
+                startActivity(_intent);
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);//update location every second
+
+
+    }
+
+    public void exitComm(View view){//exit returns to the main screen/activity
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+    //then print output on xml file. Do in while loop
+
+}
---
+++ app/src/main/java/kevinhunte/speedometer/MainActivity.java
@@ -0,0 +1,100 @@
+package kevinhunte.speedometer;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;//used to access gps
+    private LocationListener locationListener;
+    private TextView text;
+    private TextView text2;
+    private TextView text3;
+    private float speed;
+    private float max_speed;
+    private float avg_speed;
+    //private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView4);//ID of the widget to input speed value
+        text2 = (TextView) findViewById(R.id.textView6);//ID of widget for max speed val
+        text3 = (TextView) findViewById(R.id.textView8);//ID of widget for avg speed
+
+        max_speed=0;//initialized at zero
+
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {//if the gps location changes
+                speed = location.getSpeedAccuracyMetersPerSecond();//returns speed
+                if(speed>max_speed){ //sets max speed
+                    max_speed = speed;
+                }
+                if(speed<0.5){//will update when user is no longer moving. Too sensitive to ever get zero
+                    text.setText("Not Moving");
+                }
+                avg_speed = max_speed/2;//placed here so is constantly updated
+                text.setText(speed+" m/s");//sets this textView to now be the speed value
+                text2.setText(max_speed+" m/s");
+                text3.setText(avg_speed+" m/s");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {//if gps is turned off, offer option to turn on
+                Intent _intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);//goes to settings
+                startActivity(_intent);
+                //MAKE LESS SUDDEN
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{//permissions needed for gps
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    //Manifest.permission.ACCESS_COARSE_LOCATION,
+                    //Manifest.permission.INTERNET
+            }, 5);//5 is arbitrary
+            return;
+        }
+
+        locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);//updates location as frequently as possible
+    }
+
+    public void startMessage(View view){//action taken when start button is pressed.
+        //View specifies that something was clicked
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //Intents are used to bind windows, activities together
+        //EditText editText = (EditText) findViewById(R.id.editText); //not working properly
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);//launches new intent, which opens new activity(window)
+
+    }
+
+}
